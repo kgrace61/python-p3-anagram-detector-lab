3d5d10f0a25eeb5a8f293a0f07bb64719d123b85
kgrace61 <110522366+kgrace61@users.noreply.github.com>
2024-04-04 23:08:12 -0500

    lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
-# your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, possible_anagrams):
+        sorted_word = sorted(self.word)
+        
+        matches =[]
+        
+        for candidate in possible_anagrams:
+            if candidate != self.word and sorted(candidate) == sorted_word:
+                matches.append(candidate)
+        return matches
+            
+        
+        
+    
+    #takes a list of possible anagrams
+    #words in list have the same letters as word given
+    #how do i iterate over the list of words to see if the letters match? use sorted function
+    #if word matches a word in list, add to list "matches"
+    #if matches exist, or if matches is bigger than an empty list, return it
+    #if no matches, return empty list
